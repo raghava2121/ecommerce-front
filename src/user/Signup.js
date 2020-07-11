83618c3f2519fb8e8c27aac3252055b46c8c6da9
raghava bhairwade <raghavabhairwade@raghavas-MacBook-Pro.local>
2020-07-11 19:08:04 +0100

    route config

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Layout from '../core/Layout'
+import {API} from '../config'
 
-const Signup = () =>(<Layout title="My signup " description=" signup here to node ecommerce app"></Layout>
+const Signup = () =>(<Layout title="My signup " description=" signup here to node ecommerce app" >
+    <div> {API}</div>
+   
+</Layout>
 )
 
 export default Signup
